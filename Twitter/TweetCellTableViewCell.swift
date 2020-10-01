7fdbb20f4c07a2dce9c995f5acfee4241757068a
Mobin <68511123+cmobin@users.noreply.github.com>
2020-09-30 20:18:38 -0400

    Version1

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Mobin Uddin Chowdhury on 9/30/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
